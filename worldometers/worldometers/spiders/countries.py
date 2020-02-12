99d45d5eb8b1f10e62452489713ba946cf2f0c68
Josh Lipe-Melton <jlipem@gmail.com>
2020-02-12 16:43:16 -0600

    countries spider now yields country name, year, and population

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -10,10 +11,24 @@ class CountriesSpider(scrapy.Spider):
     def parse(self, response):
         countries = response.xpath("//td/a")
         for country in countries:
-            # name = country.xpath(".//text()").get()
+            name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
             # absoulte_url = f"https://www.worldometers.info{link}"
             # absoulte_url = response.urljoin(link)
 
-            yield response.follow(url=link)
+            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name})
+    
+    def parse_country(self, response):
+        # logging.info(response.url)
+        country_name = response.request.meta['country_name']
+        rows = response.xpath("//table[@class='table table-striped table-bordered table-hover table-condensed table-list'][1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1].text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield {
+                'country_name': country_name,
+                'year': year,
+                'population': population
+            }
+    
